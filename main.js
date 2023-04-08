1a08719fd13ad74dade5551306af4f4df813bdb8
Galatianu Mihai <mgalatianu@yahoo.ro>
2023-04-08 22:06:42 +0300

    searchPage

--- main.js
+++ main.js
@@ -4,3 +4,45 @@ hamburger.addEventListener('click',function() {
     this.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-open');
 });
+
+function generateChart() {
+  // Obțineți containerul pentru grafic
+  var chartContainer = document.getElementById("results-container");
+
+  // Definește datele de intrare pentru grafic
+  var dataPoints = [
+    { label: "Canabis", y: 10 },
+    { label: "Cocaina", y: 15 },
+    { label: "Ecstasy", y: 25 },
+    { label: "Heroina", y: 20 },
+    { label: "Metamfetamina", y: 5 },
+  ];
+
+  // Definește opțiunile pentru grafic
+  var options = {
+    title: {
+      text: "Statistici despre tipurile de droguri",
+    },
+    width: 800,
+    height: 600,
+    data: [
+      {
+        type: "column",
+        dataPoints: dataPoints,
+      },
+    ],
+  };
+
+  // Generează graficul folosind opțiunile și containerul specificat
+  var chart = new CanvasJS.Chart(chartContainer, options);
+
+  chart.render();
+}
+
+// Obțineți butonul de căutare
+var searchButton = document.getElementById("search-button");
+
+// Adăugați un eveniment de click la butonul de căutare
+searchButton.addEventListener("click", function () {
+  generateChart();
+});
