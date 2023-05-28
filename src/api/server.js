7e9d7a9a22be6d65979524b36754c6738c647b11
GeorgeDenis <george.duluta@yahoo.com>
2023-05-28 18:02:28 +0300

    RateLimiter

--- src/api/server.js
+++ src/api/server.js
@@ -2,18 +2,35 @@ const http = require("http");
 const apiRequest = require("./controllers/controller");
 const handleViewRequest = require("./controllers/pagesController");
 
+const rateLimitWindowTimeInMillis = 60000;
+const maxRequestsPerWindow = 500;
+const requestsLog = new Map();
+
 const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url.startsWith("/api")) {
-    apiRequest(req, res);
+  const currentTime = Date.now();
+  const clientIp = req.socket.remoteAddress;
+
+  const requests = requestsLog.get(clientIp) || [];
+  const requestsInWindow = requests.filter(
+    (time) => currentTime - time < rateLimitWindowTimeInMillis
+  );
+
+  if (requestsInWindow.length >= maxRequestsPerWindow) {
+    res.writeHead(429, { "Content-Type": "text/plain" });
+    res.end("Too many requests, please try again later.");
   } else {
-    handleViewRequest(req, res);
+    requests.push(currentTime);
+    requestsLog.set(clientIp, requests);
+
+    const url = req.url;
+    if (url.startsWith("/api")) {
+      apiRequest(req, res);
+    } else {
+      handleViewRequest(req, res);
+    }
   }
-  // else {
-  //   res.statusCode = 404;
-  //   res.end("Not found");
-  // }
 });
+
 server.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
