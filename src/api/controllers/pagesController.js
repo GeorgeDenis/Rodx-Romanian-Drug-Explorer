c965cb4d8ba2cfbc594d55a968d258aea19e5528
Carp Marcel <ycarp32@yahoo.com>
2023-06-22 01:10:42 +0300

    404PageNotFound

--- src/api/controllers/pagesController.js
+++ src/api/controllers/pagesController.js
@@ -12,24 +12,27 @@ const mimeTypes = {
   ".txt": "text/plain",
   ".gif": "image/gif",
 };
-
 const respondFile = async (req, res, filePath) => {
+  const ext = path.extname(filePath);
+
   try {
+
     const data = await fs.readFile(filePath);
-    const ext = path.extname(filePath);
     res.setHeader("Content-Type", mimeTypes[ext] || "text/plain");
     res.end(data);
   } catch (err) {
     if (err.code === "ENOENT") {
-      res.statusCode = 404;
-      res.end("Not Found");
+        const errorData = await fs.readFile("./view/404.html");
+        res.setHeader("Content-Type", "text/html");
+        res.statusCode = 404;
+        res.end(errorData);
+       
     } else {
       res.statusCode = 500;
       res.end("Internal Server Error");
     }
   }
 };
-
 const handleViewRequest = (req, res) => {
   let filePath;
   const { method, url } = req;
